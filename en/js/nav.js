d91e299684bf2d0e1b6953ac4d7f2a9f29d6292e
Tracy Tung <d820430@yahoo.com.tw>
2019-10-15 15:41:06 +0800

    test01

---
+++ en/js/nav.js
@@ -0,0 +1,44 @@
+  $(document).ready(function() {
+
+	$('a#toggle').on('click',function(){
+		$('nav').toggle( "fade", 600 );
+		$('.menubars01').toggleClass('rotate01');
+		$('.menubars02').toggleClass('none');
+		$('.menubars').toggleClass('rotate02');
+
+
+	});
+
+
+	$('.nav-background li').on('mouseover',function(){
+		$(this).addClass('hover_li');
+	});
+
+	$(window).on('scroll',function(){
+
+
+		//bakc to top
+		if( ($(window).scrollTop() >= $('.index02').offset().top) & ($(window).width() >= 420) ){
+			// alert('ddd')
+			$('.back-top').show( "fade", 1000 );
+		}
+
+		else{
+			$('.back-top').hide("fade", 1000 )
+		}
+
+
+	});
+
+	$('.back-top').on('click',function(){
+		// alert('xa');
+		$('html,body').animate({scrollTop:0},'500');
+	});
+
+	$('img.logo').on('click',function(){
+		// alert('xa');
+		$('html,body').animate({scrollTop:0},'500');
+	});
+
+
+});
